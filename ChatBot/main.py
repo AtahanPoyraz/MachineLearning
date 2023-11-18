ef621172ec2e5b5ecb40e877dae75b261ecb11ab
Atahan Poyraz <atahanpoyraz@gmail.com>
2023-11-18 23:57:06 +0300

    Update main.py

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -6,55 +6,71 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import time
 
-def prediction(girdi, model):
-    try:
-        tahmin = model.predict([girdi])
-        cevap = tahmin[0]
-        if cevap:
-            print("\x1b[1;32m[+]\x1b[1;0m FRIDAY")
-            for harf in cevap:
-                print(harf, end="", flush=True)
-                time.sleep(0.06)
+class FRIDAY:
+    def __init__(self, dataset_path: str, stopwords_path : str):
+        self.current_answer = None
+        self.df = pd.read_csv(dataset_path)
+        with open(stopwords_path, "r", encoding="utf-8") as file:
+            self.stop_words = file.read().split("\n")
+
+        self.X = self.df["soru"] 
+        self.y = self.df["cevap"] 
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+
+        self.model = make_pipeline(TfidfVectorizer(stop_words=self.stop_words), RandomForestClassifier())
+        self.model.fit(self.X_train, self.y_train)
+
+    def prediction(self, girdi):
+        try:
+            tahmin = self.model.predict([girdi])
+            cevap = tahmin[0]
+
+            if cevap != self.current_answer:
+                print("\x1b[1;32m[+]\x1b[1;0m FRIDAY")
+                
+                for harf in cevap:
+                    print(harf, end="", flush=True)
+                    time.sleep(0.06)
+                
+                print("")
+                self.current_answer = cevap
+            else:
+                print("\x1b[1;31m[-]\x1b[1;0m FRIDAY")
+                print("Uzgunum Bu soru için gerekli bilgiye sahip degilim.")
+        
+        except KeyboardInterrupt:
             print("")
-        else:
-            print("\n\x1b[1;31m[-]\x1b[1;0m Model cevap veremedi.\n")
-    except KeyboardInterrupt:
-        pass
-    except Exception as e:
-        print("\n\x1b[1;31m[-]\x1b[1;0m Hata: {}".format(str(e)))
+            pass
+        
+        except Exception as e:
+            print("\n\x1b[1;31m[-]\x1b[1;0m Hata: {}".format(str(e)))
 
-df = pd.read_csv("datasets/dataset.csv")
+    def learn_mode(self):
+        print("Çıkış için 'çıkış' yazınız")
+        while True:
+            soru = input("SORU: ")
+            if soru.lower() == "çıkış":
+                break
+            cevap = input("CEVAP: ")
+            if soru == "" and cevap == "":
+                print("Geçersiz bilgi")
+            data = f"\n{soru},{cevap.capitalize()}"
+            with open("datasets/dataset.csv", "a", encoding="utf-8") as file:
+                file.write(data)
+            print("Bilgi eklendi..")
 
-with open("stopword/stopword.txt", "r", encoding="utf-8") as file:
-    turkish_stop_words = file.read().split("\n")
+    def run(self):
+        while True:
+            try:
+                kullanici_girisi = input(f"\x1b[1;34m[x]\x1b[1;0m {os.getlogin().capitalize()}\n>> ")
+                if kullanici_girisi == "OGRENIM MODU":
+                    self.learn_mode()
+                else:
+                    self.prediction(girdi=kullanici_girisi)
+            except KeyboardInterrupt:
+                break
 
-X = df["soru"]
-y = df["cevap"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-model = make_pipeline(TfidfVectorizer(stop_words=turkish_stop_words), RandomForestClassifier())
-model.fit(X_train, y_train)
-
-while True:
-    try:
-        kullanici_girisi = input(f"\x1b[1;34m[x]\x1b[1;0m {os.getlogin().capitalize()}\n>> ")
-
-        if kullanici_girisi == "OGRENIM MODU":
-            print("Cıkış için 'çıkış' yazınız")
-            while True:
-                soru = input("SORU: ")
-                if soru.lower() == "çıkış":
-                    break
-                cevap = input("CEVAP: ")
-                if soru == "" and cevap == "":
-                    print("Gecersiz bilgi")
-                data = f"\n{soru},{cevap.capitalize()}"
-                with open("datasets/dataset.csv", "a", encoding="utf-8") as file:
-                    file.write(data)
-                print("Bilgi eklendi..")
-
-        else:
-            prediction(girdi=kullanici_girisi, model=model)
-    except KeyboardInterrupt:
-        break
+if __name__ == "__main__":
+    friday = FRIDAY(dataset_path="datasets/dataset.csv", stopwords_path="stopword/stopword.txt")
+    friday.run()
